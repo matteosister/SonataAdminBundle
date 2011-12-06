364e62c909d4d7174c4e2e7e01bdf744446d5266
matteosister <matteog@gmail.com>
2011-12-06 23:20:08 +0100

    nested set twig templates and jstree library

---
+++ Resources/public/jstree/jstree_trigger.js
@@ -0,0 +1,17 @@
+/**
+ * User: matteo
+ * Date: 06/12/11
+ * Time: 22.35
+ * @matteosister
+ * Just for fun...
+ */
+
+$(document).ready( function() {
+    $('.jstree').jstree({
+        core: {},
+        themes: {
+            theme: 'sonata'
+        },
+        plugins: ['themes', 'html_data', "checkbox", "ui"]
+    });
+});
