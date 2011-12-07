848d09943c56f1cd41eaee2399aaaba3907a8618
matteosister <matteog@gmail.com>
2011-12-07 16:29:54 +0100

    open

--- Resources/public/jstree/jstree_trigger.js
+++ Resources/public/jstree/jstree_trigger.js
@@ -12,6 +12,24 @@ $(document).ready( function() {
         themes: {
             theme: 'sonata'
         },
-        plugins: ['themes', 'html_data', "checkbox", "ui"]
+        "dnd" : {
+            "drop_finish" : function () {
+                alert("DROP");
+            },
+            "drag_check" : function (data) {
+                if(data.r.attr("id") == "phtml_1") {
+                    return false;
+                }
+                return {
+                    after : false,
+                    before : false,
+                    inside : true
+                };
+            },
+            "drag_finish" : function (data) {
+                alert("DRAG OK");
+            }
+        },
+        plugins: ['themes', 'html_data', "contextmenu", "dnd"]
     });
 });
